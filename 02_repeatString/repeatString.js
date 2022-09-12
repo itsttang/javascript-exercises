84252ff1fb29ea26d53a91306fa77920d985d5bd
itsttang <thtang57@gmail.com>
2022-09-12 10:19:49 -0400

    Create loop for string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+function repeatString(string,num) {
+    var repeatedString = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+while (num > 0) {
+    repeatedString += string;
+    num--;
+}
+    return repeatedString;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
