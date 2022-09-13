76d24cbb09526cc2e3cf8f6518dc54b8dcbb97a1
itsttang <thtang57@gmail.com>
2022-09-13 15:56:04 -0400

    Create sumAll function for first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+function sumAll(a, b) {
+let sumAll = 0;
+    for (let i = 1; i <= 4; i++) {
+    result = sumAll += i;
+}
+return result;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
