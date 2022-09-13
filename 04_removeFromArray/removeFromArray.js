642f8bbff68607729aaf52e3d5284c2109b529dc
itsttang <thtang57@gmail.com>
2022-09-13 16:24:29 -0400

    Change variable name to be easily recognizable

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
+let myArray = [1, 2, 3, 4];
 
-};
+removeFromArray = myArray.filter(number => number != 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
