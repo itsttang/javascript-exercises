35b6b50347f4c170f40b2f1823b7f9e510c8768b
itsttang <thtang57@gmail.com>
2022-09-13 16:50:25 -0400

    Create if statements for negative and non-parameter conditions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,10 @@
 const sumAll = function(min, max) {
-        if (min > max) {
-            temp = min;
-            min = max;
-            max = temp;
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        temp = min;
+        min = max;
+        max = temp;
 }
 let sumAll = 0;
 for (let i = min; i <= max; i++) {
