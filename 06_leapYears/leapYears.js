22397464b1018934bddb073436115bc7e171d812
itsttang <thtang57@gmail.com>
2022-09-14 11:48:28 -0400

    Add return statement for first test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,9 @@
 const leapYears = function(year) {
     if (year % 4 === 0) {
+        return true;
     }
 
+console.log(leapYears(1996));
 
 // Do not edit below this line
 module.exports = leapYears;
